6940104d44496d694c124e0155d5f84a1fddcb32
Andreas_Sujono <andreassujono@gmail.com>
2020-02-17 03:10:07 +0800

    Updates

---
+++ precache-manifest.51de1ac43d512e9fcf1b2e0414792841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e9cdb44f5ad2af402d4ff18136bc87",
+    "url": "/Capture/index.html"
+  },
+  {
+    "revision": "f5a2db79087736706098",
+    "url": "/Capture/static/css/main.6fa89b4c.chunk.css"
+  },
+  {
+    "revision": "c80941a042313223e5ad",
+    "url": "/Capture/static/js/2.b479e57a.chunk.js"
+  },
+  {
+    "revision": "240115190a65153634d302dfc5d801d1",
+    "url": "/Capture/static/js/2.b479e57a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5a2db79087736706098",
+    "url": "/Capture/static/js/main.b7803424.chunk.js"
+  },
+  {
+    "revision": "f7060bc0c4e25c5db80d",
+    "url": "/Capture/static/js/runtime-main.616fed2e.js"
+  }
+]);
